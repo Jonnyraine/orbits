ceb6976d44eab37c05c09084ee7ca10e622e8dd0
Jonnyraine <33321902+Jonnyraine@users.noreply.github.com>
2017-11-09 13:27:31 +0000

    Add files via upload

---
+++ Orbits/Body.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp1
+{
+    class Body
+    {
+        double size;
+        double mass;
+
+        public double Size
+        {
+            get { return this.size; }
+            set { this.size = Size; }
+        }
+
+        public double Mass
+        {
+            get { return this.mass; }
+            set { this.mass = Mass; }
+        }
+
+    }
+}
---
+++ Orbits/MainWindow.xaml.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Point startPoint = new Point();
+        Point currentPoint = new Point();
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        /**
+         * Check whether the mouse has clicked on the canvas.
+         */
+        private void Canvas_MouseDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            if (e.ButtonState == MouseButtonState.Pressed)
+            {           
+                startPoint = e.GetPosition(this);
+                Start.Content = startPoint;
+            }
+        }
+
+        /**
+         * Change the size of the circle based on how far the cursor has moved.
+         */
+        private void Canvas_MouseMove(object sender, System.Windows.Input.MouseEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Pressed)
+            {
+                Ellipse body = new Ellipse();
+                currentPoint = e.GetPosition(this);
+                if (currentPoint.X > startPoint.X)
+                {
+                    body.Width = currentPoint.X - startPoint.X;
+                    Current.Content = currentPoint;
+                }
+                else
+                {
+                    body.Width = startPoint.X - currentPoint.X;
+                    Current.Content = currentPoint;
+                }
+                body.Fill = new SolidColorBrush(Colors.Yellow);
+                Canvas.SetLeft(body, startPoint.X);
+                Canvas.SetTop(body, startPoint.Y);
+
+                DrawingCanvas.Children.Add(body);
+            }
+        }
+
+        private Color getRandomColour()
+        {
+            Random randomNumber = new Random();
+            return Color.FromArgb((byte)randomNumber.Next(255), (byte)randomNumber.Next(255), (byte)randomNumber.Next(255), (byte)randomNumber.Next(255));            
+        }
+    }
+}
